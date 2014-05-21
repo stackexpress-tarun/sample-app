5ca825bc7fcb1277f8b763322aadbd1e6930a285
Tarun Garg <tarun.garg@stackexpress.com>
2014-05-21 15:24:05 +0530

    add routes to myapp

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  get 'home/index'
-  root "home#index"
+   scope '/sample_app' do
+    get 'home/index'
+    root "home#index"
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
@@ -56,4 +57,5 @@ Rails.application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
+  end
 end
